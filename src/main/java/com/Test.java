dedaf5f9687d06527238c2e5added44ffcb74b4d
chengqiu <chengqiu@inca.com.cn>
2018-07-19 18:30:09 +0800

    添加postgresql jar包

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,26 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String host = "jdbc:postgresql://localhost:5432/bugsaas";
+	    String username = "saas_ibs_dev";
+	    String password = "saas_ibs_dev";
+	    Connection c = null;
+	    try {
+	        Class.forName("org.postgresql.Driver");
+	        c = DriverManager.getConnection(host, username, password);
+	        c.prepareStatement("select * FROM information_schema.sql_features;");
+	       
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        System.err.println(e.getClass().getName() + ": " + e.getMessage());
+	        System.exit(0);
+	    }
+	    System.out.println("Opened database successfully");
+	}
+
+}
