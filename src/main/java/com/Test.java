5c578bb7c3b8e2bf7134c359256223e3fee93ea9
chengqiu <chengqiu@inca.com.cn>
2018-08-17 17:29:23 +0800

    修改日期工具类

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -6,21 +6,23 @@ import java.sql.DriverManager;
 public class Test {
 
 	public static void main(String[] args) {
-		String host = "jdbc:postgresql://localhost:5432/bugsaas";
-	    String username = "saas_ibs_dev";
-	    String password = "saas_ibs_dev";
-	    Connection c = null;
-	    try {
-	        Class.forName("org.postgresql.Driver");
-	        c = DriverManager.getConnection(host, username, password);
-	        c.prepareStatement("select * FROM information_schema.sql_features;");
-	       
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        System.err.println(e.getClass().getName() + ": " + e.getMessage());
-	        System.exit(0);
-	    }
-	    System.out.println("Opened database successfully");
+//		String host = "jdbc:postgresql://localhost:5432/bugsaas";
+//	    String username = "saas_ibs_dev";
+//	    String password = "saas_ibs_dev";
+//	    Connection c = null;
+//	    try {
+//	        Class.forName("org.postgresql.Driver");
+//	        c = DriverManager.getConnection(host, username, password);
+//	        c.prepareStatement("select * FROM information_schema.sql_features;");
+//	       
+//	    } catch (Exception e) {
+//	        e.printStackTrace();
+//	        System.err.println(e.getClass().getName() + ": " + e.getMessage());
+//	        System.exit(0);
+//	    }
+//	    System.out.println("Opened database successfully");
+		String a="1,2,3,5,6,8,";
+		System.out.println(a.substring(0,a.length()));
 	}
 
 }
